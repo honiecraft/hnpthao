71c594d514bec224a151472c74040e8ee55d730e
hnpthao <pthao.huynhn@gmail.com>
2022-07-20 16:10:55 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>LAB02_1</h1>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
